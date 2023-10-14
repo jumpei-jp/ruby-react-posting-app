cad0566e6441b9b4616d977f8058fe4143e9b04b
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2023-10-14 12:31:49 +0900

    add: test api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :test, only: %i[index]
+    end
+  end
+
   mount_devise_token_auth_for 'User', at: 'auth'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
