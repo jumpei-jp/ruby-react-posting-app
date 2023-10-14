df32012f94355c622f2a8c4b407626aaafdc3e50
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2023-10-14 22:16:42 +0900

    add: アカウント作成とログインまでのapi実装

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :test, only: %i[index]
+      mount_devise_token_auth_for 'User',
+                                  at: 'auth',
+                                  controllers: {
+                                    registrations: 'api/v1/auth/registrations'
+                                  }
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
-
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :users, only: :index
-  resources :test_users, only: :index, :path => '/test-users'
 end
